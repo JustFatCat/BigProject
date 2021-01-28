ae69a7c85578ff1509ae66b846843925568fd162
Natalis <natalis200623@gmail.com>
2021-01-28 18:28:13 +0300

    Our solar system is ready to be deployed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        SolarSystem FactorX = new SolarSystem();
+        FactorX.sun = 1;
+        FactorX.moon = 2;
+        FactorX.planets = 2;
+
     }
 }
